75f48e359231737b15bdb4d58d110f16daa34cc4
Subramanian <selvags199@gmail.com>
2024-12-14 19:28:57 +0530

    Added Health Checks

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Play.Catalog.Service.Entities;
+using Play.Common.HealthChecks;
 using Play.Common.Identity;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
@@ -55,6 +56,9 @@ namespace Play.Catalog.Service
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Catalog.Service", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                    .AddMongoDbHealthCheck();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -83,6 +87,7 @@ namespace Play.Catalog.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapPlayEconomyHealthChecks();
             });
         }
     }
